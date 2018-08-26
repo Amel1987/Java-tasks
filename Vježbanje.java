5830c0ff315c66f28457551a6ddd905dac16b790
Amel1987 <amelcatic2012@gmail.com>
2018-08-26 12:53:34 +0200

    Add files via upload

---
+++ Vježbanje.java
@@ -0,0 +1,21 @@
+package Paket;
+
+public class Vježbanje {
+
+	public static void main(String[] args) {
+		String prvi = new String("Zagreb");
+		String drugi = new String("Zagreb");
+		
+		if(prvi == drugi)
+		{
+			System.out.println("Jednaki su");
+		}
+		else
+		{	
+			System.out.println("Nisu jednaki");
+		}
+	}
+		
+	}
+
+
---
+++ scanner1.java
@@ -0,0 +1,17 @@
+package Paket;
+import java.util.Scanner;
+
+public class scanner1 {
+
+	public static void main(String[] args) {
+		Scanner fromKey = new Scanner(System.in);
+		
+		System.out.print("Please input an integer--> ");
+		double num = fromKey.nextDouble();
+		
+		System.out.println("The number you entered is : " + num);		
+		// TODO Auto-generated method stub
+
+	}
+
+}
